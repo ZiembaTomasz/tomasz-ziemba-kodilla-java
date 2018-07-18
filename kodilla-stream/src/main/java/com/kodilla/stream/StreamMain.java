af556ff037088652f833f1a5bc0a1a2e814d9ae7
ziembatomasz <ziembatomasz@gmail.com>
2018-07-18 16:27:53 +0200

    final

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 
@@ -11,5 +12,7 @@ public class StreamMain {
         poemBeautifier.beutify("Quo Vadis", (poem) -> poem.toUpperCase());
         poemBeautifier.beutify("VENI, VIDI, VICI", (poem) -> poem.toLowerCase());
         poemBeautifier.beutify("Take it easy", (poem) -> poem.substring(7));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
