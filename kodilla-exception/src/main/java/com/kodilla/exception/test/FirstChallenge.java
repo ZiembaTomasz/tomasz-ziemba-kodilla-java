06652a66abc520e23186522a583336f4f2d26676
ziembatomasz <ziembatomasz@gmail.com>
2018-07-26 16:13:21 +0200

    bledy

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -4,16 +4,10 @@ import java.io.IOException;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if(b == 0) {
-                throw new ArithmeticException();
-            }
-            return a / b;
-        }finally{
-            System.out.println("nie dziel przez zeroo");
+        if(b == 0){
+            throw new ArithmeticException();
         }
-
+        return a / b;
     }
 
     /**
@@ -23,10 +17,19 @@ public class FirstChallenge {
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }catch(ArithmeticException e) {
+            System.out.println("nigdy nie dziel przez zero");
+        }finally{
+            System.out.println("Zasady Arytmetyki");
+        }
     }
 }
+
+
+
+
+
+
