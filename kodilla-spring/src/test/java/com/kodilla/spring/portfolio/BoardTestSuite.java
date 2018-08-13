48d4a2dc5f3f5c42fa0a95366fc67c170af0a688
ziembatomasz <ziembatomasz@gmail.com>
2018-08-13 21:51:11 +0200

    board

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+public class BoardTestSuite {
+    @Autowired
+    private Board board;
+    @Test
+    public void testTaskAdd(){
+
+    }
+}
